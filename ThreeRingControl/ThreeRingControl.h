539222aadda8250de39e99dccb2deb2e258308a4
hamed akhlaghi <akhlaghi65@gmail.com>
2017-09-06 10:05:22 +0430

    First

---
+++ ThreeRingControl/ThreeRingControl.h
@@ -0,0 +1,19 @@
+//
+//  ThreeRingControl.h
+//  ThreeRingControl
+//
+//  Created by hamed akhlaghi on 6/13/1396 AP.
+//  Copyright © 1396 nuesoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ThreeRingControl.
+FOUNDATION_EXPORT double ThreeRingControlVersionNumber;
+
+//! Project version string for ThreeRingControl.
+FOUNDATION_EXPORT const unsigned char ThreeRingControlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ThreeRingControl/PublicHeader.h>
+
+
